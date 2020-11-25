bba83e552c7ac837d5eccfeb00cf558ac96a1742
大泽 <q781000648@qq.com>
2020-11-25 12:39:37 +0800

    JavaScript基础

---
+++ 03/js/01.js
@@ -0,0 +1,9 @@
+var heigh = 98.5;
+var name = "张三";
+var xb = "男";
+console.log("最高分: " + heigh);
+console.log("姓名: " + name);
+console.log("性别: " + xb);
+document.write("最高分: " + heigh + "<br>");
+document.write("姓名: " + name + "<br>");
+document.write("性别: " + xb + "<br>");
